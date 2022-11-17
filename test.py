0f54809c7f13a5c8ab356faeca38318d4a9b1523
Bikram <bkrmprkt@gmail.com>
2022-11-18 01:58:52 +0530

    count blue pixel

--- test.py
+++ test.py
@@ -1,17 +1,15 @@
-from PIL import Image, ImageChops
+import cv2
+import numpy as np
 
+img = cv2.imread("dataset/1.jpg")
+# minimum value of blue pixel in BGR order
+BLUE_MIN = np.array([200, 0, 0], np.uint8)
+# maximum value of blue pixel in BGR order
+BLUE_MAX = np.array([255, 50, 50], np.uint8)
 
-# assign images
-
-img1 = Image.open("dataset/4.jpg")
-
-img2 = Image.open("dataset/5.jpg")
-
-
-# finding difference
-
-diff = ImageChops.difference(img1, img2)
-
-output = diff.save('output.jpg')
-
-diff.show()
+dst = cv2.inRange(img, BLUE_MIN, BLUE_MAX)
+no_blue = cv2.countNonZero(dst)
+print('The number of blue pixels is: ' + str(no_blue))
+cv2.namedWindow("opencv")
+cv2.imshow("opencv", img)
+cv2.waitKey(0)
