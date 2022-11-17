82a6cd0a5f4c61f36267c4b141caedbe89896fe1
Bikram <bkrmprkt@gmail.com>
2022-11-18 01:32:46 +0530

    automate whatsapp

--- test.py
+++ test.py
@@ -1,1 +1,12 @@
-print("Hello World!")
+import pyautogui
+import time
+
+time.sleep(5)
+
+text = 'kire bhai code patha'
+
+for i in range(10):
+    pyautogui.typewrite(text)
+
+    time.sleep(2)
+    pyautogui.press('enter')
